e899ebb60382c4822b066d538a0cebd66dd769e8
infosreenus <infosreenus@gmail.com>
2023-10-02 16:14:26 +0530

    Sreenu

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 export default function Home(props) {
-    console.log(props)
   return (
     <div>
         <h1>{props.title}</h1>
         <p>{props.content}</p>
+        <span>Sreenu</span>
     </div>
   )
 }
